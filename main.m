e0b9e0d512b1b8e55e39c84bf2cf80e5f1f632fa
Hunter Quebedeaux <hunterq@knights.ucf.edu>
2023-04-24 15:45:57 -0400

    added disturbance and control bounds

--- main.m
+++ main.m
@@ -7,7 +7,7 @@ tSim = 550;
 %Cl,Cd?
 % x0 = load('scaled.mat','x0');
 % params = load('scaled.mat','params');
-%%-------------------------------Parameters------------------------------%%
+%% -------------------------------Parameters------------------------------%%
 % taken from chapter 12 from hicks atmospheric reentry
 beta = 0.14;            %inverse scale height [km^-1] (page 381)
 r_e = 6378.137;         %earth radius [km] (page 381)
@@ -35,13 +35,13 @@ v0 = v0*1000;       %[m/s]
 % rho_s = rho_s/(1000)^3;
 % g_s = g_s/1000;
 
-%%------------------------------Equations--------------------------------%%
+%% ------------------------------Equations--------------------------------%%
 %g = g_s*(r_e/r)^2;                 %gravity equation
 %rho = rho_s*exp(-beta*(r-r_e));    %density equation
 %L = 0.5*(rho*Cl*S*V^2);            %lift force equation
 %D = 0.5*(rho*Cd*S*V^2);            %drag force equation
 
-%%----------------------------System Dynamics----------------------------%%
+%% ----------------------------System Dynamics----------------------------%%
 % x0 = load('scaled.mat','x0');
 % params = load('scaled.mat','params');
 % x0 = struct2array(x0);
@@ -61,7 +61,7 @@ R = eye(1);
 
 K = lqr(A,B,Q,R);
 
-%%----------------------------Reference Model----------------------------%%
+%% ----------------------------Reference Model----------------------------%%
 Aref =  A-B*K;
 Bref = [0;0;1];
 
@@ -69,10 +69,10 @@ Bref = [0;0;1];
 Gama = 1;
 Theta = [0 0 1];
 
-Gamax = .1*eye(3);
-Gamar = .1;
-Gamat = .1*eye(3);
-Qref = diag([.1 0 0]);
+Gamax = 1*eye(3);
+Gamar = 1;
+Gamat = 1*eye(3);
+Qref = diag([1 0 0]);
 
 P = lyap(Aref',Qref);
 
@@ -82,30 +82,53 @@ x0 = [r0;v0;gamma0];            %initial conditions
 r_des = 10 * 1000; % 10 km above the surface
 load("network.mat",'net')
 
+meanDist = 500;
+varDist = 50;
 
-tspan = linspace(0,550,1000);
-[T,X] = ode45(@(t,x) command(t,x,net),tspan,[r0;theta0;phi0;v0;gamma0;psi0]);
-% [T,X] = ode45(@(t,x) command3(t,x),tspan,[r0;v0;gamma0]);
+e0 = 0;
 
-figure
-plot(T,X(:,1)/1000-r_e/1000)
-xlabel('Time [s]')
-ylabel('Altitude [km]')
-%% 
+% tspan = linspace(0,550,1000);
+% [T,X] = ode45(@(t,x) command(t,x,net),tspan,[r0;theta0;phi0;v0;gamma0;psi0]);
+% % [T,X] = ode45(@(t,x) command3(t,x),tspan,[r0;v0;gamma0]);
+% 
+% figure
+% plot(T,X(:,1)/1000-r_e/1000)
+% xlabel('Time [s]')
+% ylabel('Altitude [km]')
 
-%%----------------------------Simulation----------------------------%%
+%% ----------------------------Simulation----------------------------%%
 sim('EDLSim');
 
 
-%%----------------------------Plotting----------------------------%%
+%% ----------------------------Plotting----------------------------%%
 
 figure
 plot(radiusTracking(:,1),(radiusTracking(:,2:end)-r_e)/1000)
 grid on
 legend('Signal','Plant','Reference Model')
 xlabel('Seconds')
-ylabel('Altitude (m)')
+ylabel('Altitude (km)')
 
 figure
 plot(u.time,squeeze(u.signals.values))
 grid on
+title('Control - Flight Path Angle Rate of Change')
+
+figure
+plot(error.time,squeeze(error.signals.values))
+grid on
+title('Error Between Reference Signal and Plant')
+xlabel('Seconds')
+ylabel('Altitude Error (m)')
+
+figure
+subplot(2,1,1)
+plot(u.time,vecnorm(squeeze(kx.signals.values),2)')
+grid on
+title('kx')
+
+subplot(2,1,2)
+plot(u.time,kr.signals.values)
+grid on
+title('kr')
+xlabel('Seconds')
